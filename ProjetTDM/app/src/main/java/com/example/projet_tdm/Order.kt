40fd81813700b51d6819e32ffefc97b4defbf31b
Redha Amirouche <hm_amirouche@esi.dz>
2021-04-03 12:50:28 +0100

    edit Order Data class

--- ProjetTDM/app/src/main/java/com/example/projet_tdm/Order.kt
+++ ProjetTDM/app/src/main/java/com/example/projet_tdm/Order.kt
@@ -1,5 +1,5 @@
 package com.example.projet_tdm
 
-data class Order(var numOrder: Int, var nameOrder: String,var statusOrder: String, var clientAdress:Localisation, var clientPhoneNumber:String, var clientEmail: String, var priceOrder: Long  ) {
+data class Order(var num: Int, var name: String,var status: String, var clientAdress:Localisation, var clientPhoneNumber:String, var clientEmail: String, var priceOrder: Long  ) {
     lateinit var productsList: List<Product>
 }
